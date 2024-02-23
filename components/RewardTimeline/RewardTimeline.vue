35fce74de310ba155134f732ecc779e17dfeb82e
tasos <lazarou.ts@gmail.com>
2024-02-23 16:02:53 +0200

    Fix height of scrollable content

--- components/RewardTimeline/RewardTimeline.vue
+++ components/RewardTimeline/RewardTimeline.vue
@@ -192,42 +192,45 @@
           color="background"
           elevation="0"
           rounded="0"
-          style="height: 100vh; overflow: scroll"
+          height="100% - 64px"
+          style="height: calc(100dvh - 64px); overflow: scroll"
         >
-          <v-infinite-scroll :items="timeline" color="primary" @load="getMoreRewards">
-            <template v-for="(item, index) in timeline" :key="index">
-              <div class="ps-7 d-flex" :class="index !== 0 ? ' align-center' : 'align-start'">
-                <div class="d-flex flex-column justify-center align-center" style="width: 12px">
-                  <VSheet
-                    v-if="index !== 0"
-                    style="height: 26px; width: 4px"
-                    color="mediumGrey"
-                    rounded="0"
-                  />
-                  <VSheet style="height: 12px; width: 12px" color="mediumGrey" rounded="circle" />
-                  <VSheet style="height: 26px; width: 4px" color="mediumGrey" rounded="0" />
+          <VCardText class="pa-0">
+            <v-infinite-scroll :items="timeline" color="primary" @load="getMoreRewards">
+              <template v-for="(item, index) in timeline" :key="index">
+                <div class="ps-7 d-flex" :class="index !== 0 ? ' align-center' : 'align-start'">
+                  <div class="d-flex flex-column justify-center align-center" style="width: 12px">
+                    <VSheet
+                      v-if="index !== 0"
+                      style="height: 26px; width: 4px"
+                      color="mediumGrey"
+                      rounded="0"
+                    />
+                    <VSheet style="height: 12px; width: 12px" color="mediumGrey" rounded="circle" />
+                    <VSheet style="height: 26px; width: 4px" color="mediumGrey" rounded="0" />
+                  </div>
+                  <div
+                    class="text-darkGrey font-weight-bold ms-4 d-flex"
+                    style="font-size: 0.984rem; line-height: 100%"
+                  >
+                    {{ dayjs(item.timestamp).format('MMM D, YYYY') }}
+                  </div>
                 </div>
-                <div
-                  class="text-darkGrey font-weight-bold ms-4 d-flex"
-                  style="font-size: 0.984rem; line-height: 100%"
-                >
-                  {{ dayjs(item.timestamp).format('MMM D, YYYY') }}
-                </div>
-              </div>
-              <DailyRewards
-                :date="item.timestamp"
-                :daily-amount="item.total_reward"
-                :validation-score-color="item.validation_color"
-                :base-reward-amount="item.base_reward"
-                :boost-amount="item.total_business_boost_reward"
-                :state="item.severity_level"
-                :has-active-boosts="item.has_active_boosts"
-              />
-            </template>
-            <template #empty>
-              <span></span>
-            </template>
-          </v-infinite-scroll>
+                <DailyRewards
+                  :date="item.timestamp"
+                  :daily-amount="item.total_reward"
+                  :validation-score-color="item.validation_color"
+                  :base-reward-amount="item.base_reward"
+                  :boost-amount="item.total_business_boost_reward"
+                  :state="item.severity_level"
+                  :has-active-boosts="item.has_active_boosts"
+                />
+              </template>
+              <template #empty>
+                <span></span>
+              </template>
+            </v-infinite-scroll>
+          </VCardText>
         </VCard>
       </VCardText>
     </VCardText>
