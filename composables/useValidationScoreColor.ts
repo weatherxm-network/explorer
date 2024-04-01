d4dbb241e067366de38b6afa8e8ccfaf916133eb
tasos <lazarou.ts@gmail.com>
2024-04-01 15:24:12 +0300

    Change validation color thresholds

--- composables/useValidationScoreColor.ts
+++ composables/useValidationScoreColor.ts
@@ -8,11 +8,11 @@ export const useValidationScoreColor = () => {
       return '#BAC7CB'
     } else {
       switch (true) {
-        case validationScore >= 0 && validationScore <= 10:
+        case validationScore >= 0 && validationScore < 10:
           return theme.current.value.colors.error
-        case validationScore > 10 && validationScore <= 95:
+        case validationScore >= 10 && validationScore < 95:
           return theme.current.value.colors.warning
-        case validationScore > 95 && validationScore <= 100:
+        case validationScore >= 95 && validationScore <= 100:
           return theme.current.value.colors.rewardVeryHigh
         default:
           return '#BAC7CB'
