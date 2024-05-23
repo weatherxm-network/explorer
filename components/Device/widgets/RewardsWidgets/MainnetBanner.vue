64e91a25907f1107166f03226255d9d332bcecd3
Tasos <lazarou.ts@gmail.com>
2024-05-23 14:00:35 +0300

    Refactor mainnet banner

--- components/Device/widgets/RewardsWidgets/MainnetBanner.vue
+++ components/Device/widgets/RewardsWidgets/MainnetBanner.vue
@@ -12,29 +12,29 @@
   const remoteConfig = await fetchRemoteConfig()
   const mainnetBannerText = ref<string>(`${remoteConfig.feat_mainnet_message._value}`)
   const mainnetUrl = ref<string>(`${remoteConfig.feat_mainnet_url._value}`)
+  const computedBannerText = computed(() => {
+    return mainnetBannerText.value.replaceAll(/\n/g, '<br/>')
+  })
 </script>
 
 <template>
-  <VImg
-    :src="bannerBackground"
-    class="rounded-xl mb-4 d-flex justify-center align-center"
-    style="cursor: pointer"
-    @click="navigateTo(mainnetUrl, { open: { target: '_blank' } })"
-  >
-    <div
-      class="d-flex flex-column text-center align-center justify-center px-1"
-      :class="theme.current.value.dark ? `text-text` : 'text-top'"
-    >
+  <a :href="mainnetUrl" target="_blank" class="text-decoration-none">
+    <VImg :src="bannerBackground" class="rounded-xl mb-4 d-flex justify-center align-center">
       <div
-        :style="smBreakpoing ? { 'font-size': '1.5rem' } : { 'font-size': '1.75rem' }"
-        class="font-weight-bold"
+        class="d-flex flex-column text-center align-center justify-center px-1"
+        :class="theme.current.value.dark ? `text-text` : 'text-top'"
       >
-        <i class="fa-regular fa-hexagon-check"></i>
-        <span class="pl-2">Welcome to Mainnet!</span>
+        <div
+          :style="smBreakpoing ? { 'font-size': '1.5rem' } : { 'font-size': '1.75rem' }"
+          class="font-weight-bold"
+        >
+          <i class="fa-regular fa-hexagon-check"></i>
+          <span class="pl-2">Welcome to Mainnet!</span>
+        </div>
+        <div class="text-body-2 my-2 px-2">
+          <div v-html="computedBannerText" />
+        </div>
       </div>
-      <div class="text-body-2 my-2">
-        <div>{{ mainnetBannerText }}</div>
-      </div>
-    </div>
-  </VImg>
+    </VImg>
+  </a>
 </template>
