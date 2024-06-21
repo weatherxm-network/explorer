153aebeed94f2ac9054d4166e1271b1400e90b38
Shiroe <aggelosteck@gmail.com>
2024-06-21 15:43:44 +0300

    updated eslint.config.mjs setup to use individual parsers based on the type and also fixed prettier config for conflicting rules of eslint and update gitignore for yarn@4.3.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,29 +1,56 @@
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-import eslintConfigPrettier from 'eslint-config-prettier'
-import vueEslintParser from 'vue-eslint-parser'
-import tsEslingParser from '@typescript-eslint/parser'
 import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginVue from 'eslint-plugin-vue'
+import vueEslintParser from 'vue-eslint-parser'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default withNuxt([
+  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...pluginVue.configs['flat/base'],
+  ...pluginVue.configs['flat/recommended'],
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.vue'],
+    files: ['**/*.{ts,vue}'],
+    plugins: {
+      'typescript-eslint': tseslint.plugin
+    },
     languageOptions: {
       parser: vueEslintParser,
       parserOptions: {
-        parser: tsEslingParser
+        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+        extraFileExtensions: ['.vue'],
+        sourceType: 'module',
+        parser: {
+          js: 'espree',
+          jsx: 'espree',
+          mjs: 'espree',
+          ts: tseslint.parser,
+          tsx: tseslint.parser,
+          mts: tseslint.parser
+          // Leave the template parser unspecified, so that it could be determined by `<script lang="...">`
+        }
       },
+      // https://eslint.vuejs.org/user-guide/#auto-imports-support
       globals: {
-        ...globals.browser,
-        ...globals.node
+        ref: 'readonly',
+        computed: 'readonly',
+        watch: 'readonly',
+        watchEffect: 'readonly'
       }
     },
-    plugins: {},
-    // add your custom rules here
     rules: {
       'vue/multi-word-component-names': 'off',
       'no-console': 'off'
     }
+  },
+  {
+    ignores: ['node_modules', 'dist', 'public', '.nuxt']
   },
   // As usual prettier config should come last in order
   // to not clash with the linter
