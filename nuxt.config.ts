3b736ad052b41ef4d9de5654fb3d8f0c1325ba62
Antzelo Balliu <aggelosteck@gmail.com>
2024-10-14 13:26:59 +0200

    fixed issue of runtime config not updating at runtime
    - The missmatch in the nuxtConfig of the variable made it so it was only
    updated at build time -> whereas if you want it to be updated at runtime
    it has to match exactly the name after the 'NUXT_PUBLIC_' in camelCase
    format

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,11 @@ import Joi from 'joi'
 import pkg from './package.json'
 
 const config = {
-  apiUrl: process.env.API_BASE_URL,
+  apiUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
   mapboxAccessToken: process.env.MAPBOX_ACCESS_TOKEN,
   mapboxStyle: process.env.MAPBOX_STYLE,
   fontAwesomeProkitKey: process.env.FONT_AWESOME_PRO_KIT_KEY,
-  initGoogleServices: process.env.INIT_GOOGLE_SERVICES === 'true'
+  initGoogleServices: process.env.INIT_GOOGLE_SERVICES === 'true',
 }
 
 const configSchema = Joi.object({
@@ -16,7 +16,7 @@ const configSchema = Joi.object({
   mapboxAccessToken: Joi.string().required(),
   mapboxStyle: Joi.string().required(),
   fontAwesomeProkitKey: Joi.string().required(),
-  initGoogleServices: Joi.boolean().required()
+  initGoogleServices: Joi.boolean().required(),
 }).strict()
 
 // Validate configuration
@@ -30,22 +30,25 @@ export default defineNuxtConfig({
   spaLoadingTemplate: false,
   ssr: false,
   build: {
-    transpile: ['vuetify']
+    transpile: ['vuetify'],
   },
-  css: ['vuetify/lib/styles/main.sass', '@mdi/font/css/materialdesignicons.min.css'],
+  css: [
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css',
+  ],
   devServer: {
-    port: 3001
+    port: 3001,
   },
   modules: ['@pinia/nuxt', 'nuxt-snackbar', '@vueuse/nuxt', '@nuxt/eslint'],
   snackbar: {
     bottom: true,
     duration: 2000,
     dense: true,
-    zIndex: 99999
+    zIndex: 99999,
   },
   runtimeConfig: {
     public: {
-      apiUrl: config.apiUrl,
+      apiBaseUrl: config.apiUrl,
       mapboxAccessToken: config.mapboxAccessToken,
       mapboxStyle: config.mapboxStyle,
       initGoogleServices: config.initGoogleServices,
@@ -59,8 +62,8 @@ export default defineNuxtConfig({
       firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
       firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
       firebaseAppId: process.env.FIREBASE_APP_ID,
-      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID
-    }
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    },
   },
   app: {
     head: {
@@ -72,29 +75,38 @@ export default defineNuxtConfig({
         { property: 'og:description', content: `People's Weather Network.` },
         { property: 'og:type', content: 'website' },
         { property: 'og:locale', content: 'en_US' },
-        { property: 'og:image', content: 'https://explorer-static.weatherxm.com/wxm-logo-512.png' },
+        {
+          property: 'og:image',
+          content: 'https://explorer-static.weatherxm.com/wxm-logo-512.png',
+        },
         { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'twitter:image', content: 'https://explorer-static.weatherxm.com/wxm-logo-512.png' }
+        {
+          name: 'twitter:image',
+          content: 'https://explorer-static.weatherxm.com/wxm-logo-512.png',
+        },
       ],
       link: [
-        { rel: 'stylesheet', href: 'https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css' },
         {
           rel: 'stylesheet',
-          href: 'https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.0.96/css/materialdesignicons.min.css'
+          href: 'https://api.mapbox.com/mapbox-gl-js/v2.15.0/mapbox-gl.css',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.0.96/css/materialdesignicons.min.css',
         },
         {
           href: `https://kit.fontawesome.com/${config.fontAwesomeProkitKey}.css`,
           rel: 'stylesheet',
-          crossorigin: 'anonymous'
+          crossorigin: 'anonymous',
         },
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       ],
       script: [
         {
           src: `https://kit.fontawesome.com/${config.fontAwesomeProkitKey}.js`,
-          crossorigin: 'anonymous'
-        }
-      ]
-    }
-  }
+          crossorigin: 'anonymous',
+        },
+      ],
+    },
+  },
 })
