df22ce537e1d8d4ffe59ebe7b1535fd5f7e3e43e
Antzelo Balliu <aggelosteck@gmail.com>
2025-03-18 15:35:42 +0100

    fe-1693 updated env naming and config for CI/CD

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,20 +32,22 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL,
-      mapboxAccessToken: process.env.MAPBOX_ACCESS_TOKEN,
-      mapboxStyle: process.env.MAPBOX_STYLE,
-      initGoogleServices: process.env.INIT_GOOGLE_SERVICES === 'true',
+      mapboxAccessToken: process.env.NUXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
+      mapboxStyle: process.env.NUXT_PUBLIC_MAPBOX_STYLE,
+      initGoogleServices:
+        process.env.NUXT_PUBLIC_INIT_GOOGLE_SERVICES === 'true',
       version: process.env.GIT_COMMIT_HASH
         ? `${pkg.version}-${process.env.GIT_COMMIT_HASH}`
         : pkg.version,
-      gtag: process.env.GOOGLE_ANALYTICS_ID,
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
-      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
+      gtag: process.env.NUXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+      firebaseApiKey: process.env.NUXT_PUBLIC_FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.NUXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.NUXT_PUBLIC_FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.NUXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId:
+        process.env.NUXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.NUXT_PUBLIC_FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.NUXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
     },
   },
   app: {
@@ -78,7 +80,7 @@ export default defineNuxtConfig({
           href: 'https://cdnjs.cloudflare.com/ajax/libs/MaterialDesign-Webfont/7.0.96/css/materialdesignicons.min.css',
         },
         {
-          href: `https://kit.fontawesome.com/${process.env.FONT_AWESOME_PRO_KIT_KEY}.css`,
+          href: `https://kit.fontawesome.com/${process.env.NUXT_PUBLIC_MAPBOX_STYLE}.css`,
           rel: 'stylesheet',
           crossorigin: 'anonymous',
         },
@@ -86,7 +88,7 @@ export default defineNuxtConfig({
       ],
       script: [
         {
-          src: `https://kit.fontawesome.com/${process.env.FONT_AWESOME_PRO_KIT_KEY}.js`,
+          src: `https://kit.fontawesome.com/${process.env.NUXT_PUBLIC_MAPBOX_STYLE}.js`,
           crossorigin: 'anonymous',
         },
       ],
