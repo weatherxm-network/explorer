e921e7ba3b544bc045fe82e6958b10c20147d295
Dimitris Theodoropoulos <dimitriss.theodoropoulos@gmail.com>
2026-02-06 18:59:06 +0200

    (feat): qr-code promo changed position (#104)

--- components/Device/widgets/Forecast.vue
+++ components/Device/widgets/Forecast.vue
@@ -1,26 +1,13 @@
 <script setup lang="ts">
-  import QRCode from 'qrcode'
-  import { useTheme } from 'vuetify'
   import promoWXMProDesktopImg from '~/assets/promo-pro-desktop.png'
   import wxmProLogoWithText from '~/assets/wxm-pro-logo-text.svg'
 
-  const QR_URL = 'https://weatherxm2-h9cuwbhka-weatherxm-1.vercel.app/api'
   const STORE_LINK = {
     apple: 'https://apps.apple.com/ca/app/weatherxm/id1629841929',
     google: 'https://play.google.com/store/apps/details?id=com.weatherxm.app',
   }
 
-  const theme = useTheme()
-
   const { trackGAevent } = useGAevents()
-  const qrCanvas = ref<HTMLCanvasElement>()
-
-  watch(
-    () => theme.current.value.dark,
-    () => {
-      generateQR(QR_URL)
-    },
-  )
 
   const openStore = (store: 'apple' | 'google') => {
     trackGAevent('deviceForecastClickOnAppStoreLink', { ITEM_ID: store })
@@ -31,27 +18,6 @@
     const WXMProLink = 'https://pro.weatherxm.com/sign-in'
     window.open(WXMProLink, '_blank')
   }
-
-  const generateQR = async (text: string) => {
-    try {
-      return QRCode.toCanvas(qrCanvas.value, text, {
-        color: {
-          dark: theme.current.value.colors.primary,
-          light: theme.current.value.colors.blueTint,
-        },
-        scale: 1,
-        width: 100,
-      })
-    } catch (e) {
-      console.error(e)
-    }
-  }
-
-  onMounted(() => {
-    if (qrCanvas.value) {
-      generateQR(QR_URL)
-    }
-  })
 </script>
 
 <template>
@@ -119,24 +85,13 @@
       </h5>
       <div
         :class="[
-          'd-flex justify-space-between align-center pa-5 pl-3 ga-1 border-thin border-opacity-100 border-primary ',
+          'pa-5 border-thin border-opacity-100 border-primary ',
         ]"
         :style="{ borderRadius: '15px' }"
       >
-        <div :style="{ width: '100px', height: '100px', aspectRatio: '1/1' }">
-          <canvas
-            ref="qrCanvas"
-            :style="{
-              aspectRatio: '1/1;',
-            }"
-          ></canvas>
-        </div>
         <div>
-          <h6 :class="['text-body-2 mb-4']">
-            Scan this QR code with your phone camera
-          </h6>
           <p :class="['text-caption']">
-            or download from browser in
+            Download from browser in
             <span
               :class="['text-decoration-underline cursor-pointer']"
               @click="() => openStore('google')"
